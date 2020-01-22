13c8e55c24f9a79387eabb886dfa279f53b6e5bf
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-01-22 11:59:20 +0330

    stack4: set.cmd -b

--- set.cmd
+++ set.cmd
@@ -2,4 +2,4 @@
 if "%path:;./node_modules/.bin=%" == "%path%" set PATH=%PATH%;./node_modules/.bin
 set SRC=.
 set DEST=./public
-mode con:cols=60 lines=5
+if "%1" neq "-b" mode con:cols=60 lines=5
--- toggle-custom-livereload-port.cmd
+++ toggle-custom-livereload-port.cmd
@@ -1,6 +1,6 @@
 @echo off
 cmd /c npm run toggleLive
-call set.cmd
+call set.cmd -b
 cmd /c rollup -c
 cmd /c htmlbilder %SRC%/html/ -o %DEST%/index.html -t index.hbs
 echo.
