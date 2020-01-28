76e2de6b924e8ac345fa56a90389d8d32c13c135
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-01-28 18:22:46 +0330

    removed boilerplate code

--- public/js/comp1.js
+++
@@ -1,9 +0,0 @@
-const ROOT = '#comp1';
-const temp = __temps('comp1');
-let $$;
-
-function init () {
-	$$ = __els(ROOT);
-}
-
-export default { init }
--- public/js/comp2/comp2.js
+++
@@ -1,9 +0,0 @@
-const ROOT = '#comp2';
-const temp = __temps('comp2');
-let $$;
-
-function init () {
-	$$ = __els(ROOT);
-}
-
-export default { init }
