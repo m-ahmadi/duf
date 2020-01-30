c4bfd03e0910b4a5af88c4b5d8f5b02fcfa1179e
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-01-30 19:22:47 +0330

    stack4: tclp.cmd

--- tclp.cmd
+++ tclp.cmd
@@ -1,8 +1,9 @@
 @echo off
+echo Toggling custom livereload port...
 cmd /c npm run toggleLive
 call set.cmd -b
 cmd /c rollup -c
 cmd /c htmlbilder %SRC%/html/ -o %DEST%/index.html -t index.hbs
 echo.
-echo done.
+echo Done.
 pause
