a543a6b463a4bf1788cc6263960809ce6a61c9d1
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-02-03 08:08:14 -0800

    w*.cmd window sizes for laptop

--- watch.cmd
+++ watch.cmd
@@ -13,10 +13,10 @@ if %errorlevel% == 1 (echo cmdow not installed. && exit /b 1)
 
 setlocal EnableDelayedExpansion
 set count=0
-set top=150
+set top=-40
 for /f %%i in ('cmdow /t') do (
-	cmdow %%i /mov 1420 !top!
-	set /a "top=!top!+110"
+	cmdow %%i /mov 870 !top!
+	set /a "top=!top!+95"
 	set /a "count=!count!+1"
 	if "!count!" == "8" goto :done
 )
