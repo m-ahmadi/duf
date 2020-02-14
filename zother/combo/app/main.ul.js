8f3d44dfdbf020901ef5e40f55de0728c0112a64
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-02-14 23:16:44 +0330

    custom combo box: went with ul

---
+++ zother/combo/app/main.ul.js
@@ -0,0 +1,145 @@
+import instruments from './ins.js';
+import tree from './tree.js';
+window.log = console.log;
+
+
+$(async function () {
+	const ins = await instruments();
+	const $jtreeEl = await tree(ins);
+	
+	// const data = ins.map(i => [i.Symbol, i.Name]);
+	const data = ins
+		.map(i => [
+			cleanFa(i.Symbol), // i.Symbol
+			cleanFa(i.Name),   // i.Name
+			''+i.YVal
+		])
+		.sort((a,b) => a[0].localeCompare(b[0], 'fa'));
+	
+	window.ins = ins;
+	
+	const cFocus = 'focus';
+	const cHide = 'hide';
+	const input = $('.combo > input:nth-child(1)');
+	const ul = $('.combo > ul:nth-child(4)');
+	
+	const xToggle = $('.combo span:nth-child(3)');
+	const filterToggle = $('.combo > svg:nth-child(2)');
+	const jtree = $jtreeEl.jstree(true);
+	let i = -1;
+	
+	$jtreeEl.on('changed.jstree', function (e, data) {
+		// el.jstree('rename_node', '1', 'new text')
+	});
+	
+	// focus on mouse move and select item on mousedown
+	ul
+	.on('mouseenter', 'li', function () {
+		i	 = $(this).index();
+		$('li', ul).removeClass(cFocus);
+		$(this).addClass(cFocus);
+	})
+	.on('mouseleave', 'li', function () {
+		$(this).removeClass(cFocus);
+	})
+	.on('mousedown', 'li', function ({which}) {
+		input.val(this.dataset.val);
+		close();
+	});
+	
+	// nav on up/down arrow, select on enter, clear on esc.
+	input.on('keydown', function (e) {
+		const key = e.which;
+		if (key !== 38 && key !== 40 && key !== 13 && key !== 27) return;
+		if (key === 13) { // enter
+			input.val( $('> li.focus', ul).data('val') );
+			close();
+			return;
+		} else if (key === 27) { // esc
+			input.val('');
+			return;
+		}
+		const lis = $('li', ul);
+		const inc = key === 38 ? -1 : key === 40 ? 1 : 0; // 38=up 40=down
+		i += inc;
+		i = i > lis.length-1 ? 0 : i < 0 ? lis.length-1 : i;
+		lis.removeClass(cFocus).eq(i).addClass(cFocus)[0].scrollIntoView({block: 'end'});
+	});
+	
+	const allRows = data.filter(i=>jtree.get_selected().includes(''+i[2])).map(i=>`
+		<li data-val="${i[0]}">
+			<div>${i[0]}</div>
+			<div>${i[1]}</div>
+		</li>
+	`);
+	
+	// open & close on focus & blur
+	input
+		// .on('blur', close)
+		.on('focus', open)
+		.on('input change', _debounce(function () {
+			const v = this.value;
+			if (v === '') {
+				open();
+				ul.html(allRows);
+			}
+			if (v.length > 1) {
+				const res = data.filter( i => i.join(' ').includes(v) && jtree.get_selected().includes(i[2]) );
+				const rgx = new RegExp(escRgx(v), 'g');
+				const replaceWith = `<span class="query">${v}</span>`;
+				ul.html(res.map(i=>`
+					<li data-val="${i[0]}">
+						<div>${i[0].replace(rgx, replaceWith)}</div>
+						<div>${i[1].replace(rgx, replaceWith)}</div>
+					</li>
+				`));
+			}
+		}, 100));
+	
+	function reset() {
+		i = -1;
+		$('li', ul).removeClass(cFocus);
+	}
+	function open() {
+		reset();
+		if ( ul.hasClass(cHide) ) ul.removeClass(cHide);
+	}
+	function close() {
+		reset();
+		if ( !ul.hasClass(cHide) ) ul.addClass(cHide);
+	}
+	
+	filterToggle.on('click', function () {
+		$jtreeEl.toggleClass('slide');
+	});
+	
+	function escRgx(str) {
+		return str.replace(/[|\\{}()[\]^$+*?.-]/g, '\\$&');
+	}
+	
+	function cleanFa(str) { /*
+		\u200B zero-width space
+		\u200C zero-width non-joiner
+		\u200D zero-width joiner
+		\uFEFF zero-width no-break space */
+		return str
+			// .replace(/[\u200B-\u200D\uFEFF]/g, ' ')
+			.replace(/\u200B/g, '')        // zero-width space
+			.replace(/\s?\u200C\s?/g, ' ') // zero-width non-joiner
+			.replace(/\u200D/g, '')        // zero-width joiner
+			.replace(/\uFEFF/g, '')        // zero-width no-break space
+			.replace(/ك/g,'ک')
+			.replace(/ي/g,'ی');
+	}
+	
+	input.trigger('change')
+});
+/* $('.combo').on('click', function (e) {
+		const { target } = e;
+		if (target.tagName === 'TD') {
+			input.val( target.innerHTML );
+			close();
+		} else if (target !== input[0]) {
+			close();
+		}
+	}); */
