8a4442e06405f1d834dc48591b46b4c1e5a07192
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-03-27 02:18:48 +0430

    sigdraw, canvas: improved axis related drawings
    corrected x,y axis labels
    added offset to make room for axis labels
    drawing separator lines to make it look more like a chart
    some improvements in drawing y labels (still has issues)
    some minor code formatting

--- zother/sigdraw/main.canvas.js
+++ zother/sigdraw/main.canvas.js
@@ -8,57 +8,77 @@ $(async function () {
 	bars = (await tse.getPrices(['فولاد']))[0];
 	closes = bars.map(i => i.close/10);
 	
-	canvas = $('canvas');
-	c = canvas[0].getContext('2d');
-	width = canvas.width();
-	height = canvas.height();
+	canvas = document.querySelector('canvas');
+	c = canvas.getContext('2d');
+	offset = 35;
+	c.canvas.width = 800;
+	c.canvas.height = 400;
+	c.canvas.width += offset;
+	c.canvas.height += offset;
+	width = c.canvas.width - offset;
+	height = c.canvas.height - offset;
 	
-	prices = closes.slice(-700);
-	
+	prices = closes.slice(-500);
 	pricesPx = map2px(prices);
 	pricesPxScaled = scale(pricesPx, 0, height);
-	
-	step = 0;
+	step = 0+offset;
 	points = pricesPxScaled.map(i => ({x: step+=1, y: i}));
 	
 	var path = new Path2D();
 	
+	// graph
 	path.moveTo(points[0].x, points[0].y);
-	points.slice(1).forEach(({x,y}) => {
-		path.lineTo(x, y);
-	});
+	points.slice(1).forEach(({x,y}) =>
+		path.lineTo(x, y)
+	);
+	c.lineWidth = 1;
+	c.strokeStyle = 'blue';
+	c.stroke(path);
 	
 	min = Math.min(...prices);
 	max = Math.max(...prices);
 	
+	// separator lines
+	c.lineWidth = 0.5;
+	c.strokeStyle = 'black';
+	c.moveTo(offset, height);
+	c.lineTo(width+offset, height);
+	c.moveTo(offset, 0);
+	c.lineTo(offset, height);
+	c.stroke();
+	
+	// horizontal lines and labels
+	c.beginPath();
 	c.lineWidth = 1;
-	c.strokeStyle = 'blue';
-	c.stroke(path);
-	c.lineWidth = 0.1;
-	c.strokeStyle = 'grey';
-	// c.setLineDash([3]);
+	c.strokeStyle = 'deeppink';
+	c.setLineDash([1]);
+	max2nd = max%100 ? max-(max%100) : max;
+	min2nd = min%100 ? min+(100-min%100) : min;
+	step = min2nd;
+	yAxLines = [min, min2nd].concat([...Array( Math.floor((max2nd-min2nd)/100) )].map(()=>step+=100), max);
+	log(yAxLines);
+	yAxLinesScaled = scale(yAxLines, 0, height);
+	yAxLinesScaled.reverse().slice(1).slice(0,-1).forEach((v,i) => {
+		c.moveTo(offset, v);
+		c.lineTo(width+offset, v);
+		c.fillText(yAxLines[i+1], 5,v+3);
+	});
+	c.fillText(yAxLines[yAxLines.length-1],5,9);
+	c.fillText(yAxLines[0],5,height-1);
+	c.stroke();
+	
+	// vertical lines and labels
+	c.beginPath();
+	c.lineWidth = 1;
+	c.strokeStyle = 'cyan';
+	c.setLineDash([1]);
 	for (let i=0; i<width; i+=30) {
-		c.moveTo(i, 0);
-		c.lineTo(i, height);
+		c.moveTo(i+offset, 0);
+		c.lineTo(i+offset, height);
 	}
 	c.stroke();
 	
-	c.beginPath();
-	c.lineWidth = 0.4;
-	c.strokeStyle = 'deeppink';
-	c.setLineDash([1]);
-	yAxLines = [...Array(Math.floor(max/100))].map((v,i) => (i+1)*100);
-	yAxLinesScaled = scale(yAxLines, 0, height);
-	yAxLinesScaled.reverse().slice(1).slice(0,-1).forEach((v,i) => {
-		c.moveTo(0, v);
-		c.lineTo(width, v);
-		c.fillText(yAxLines[i], 2,v+10);
-	});
-	c.fillText(max,2,10);
-	c.fillText(min,2,390);
-	c.stroke();
-	
-	canvas[0].addEventListener('click', function (e) {
+	canvas.addEventListener('click', function (e) {
 		// log(e.x, e.y);
 		// log( c.isPointInPath(path, e.x, e.y) );
 	});
