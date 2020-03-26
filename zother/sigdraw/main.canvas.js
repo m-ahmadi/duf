0f6ddc0ae2034b2369790f07f23614ceae21d643
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-03-27 02:16:57 +0430

    sigdraw, canvas: drawing some axis related things

--- zother/sigdraw/main.canvas.js
+++ zother/sigdraw/main.canvas.js
@@ -15,19 +15,53 @@ $(async function () {
 	
 	prices = closes.slice(-700);
 	
-	prices = map2px(prices);
-	prices = scale(prices, 0, height);
+	pricesPx = map2px(prices);
+	pricesPxScaled = scale(pricesPx, 0, height);
 	
 	step = 0;
-	points = prices.map(i => ({x: step+=1, y: i}));
+	points = pricesPxScaled.map(i => ({x: step+=1, y: i}));
+	
+	var path = new Path2D();
+	
+	path.moveTo(points[0].x, points[0].y);
+	points.slice(1).forEach(({x,y}) => {
+		path.lineTo(x, y);
+	});
+	
+	min = Math.min(...prices);
+	max = Math.max(...prices);
 	
 	c.lineWidth = 1;
 	c.strokeStyle = 'blue';
-	c.moveTo(points[0].x, points[0].y);
-	points.slice(1).forEach(({x,y}) => {
-		c.lineTo(x, y);
-	});
+	c.stroke(path);
+	c.lineWidth = 0.1;
+	c.strokeStyle = 'grey';
+	// c.setLineDash([3]);
+	for (let i=0; i<width; i+=30) {
+		c.moveTo(i, 0);
+		c.lineTo(i, height);
+	}
 	c.stroke();
+	
+	c.beginPath();
+	c.lineWidth = 0.4;
+	c.strokeStyle = 'deeppink';
+	c.setLineDash([1]);
+	yAxLines = [...Array(Math.floor(max/100))].map((v,i) => (i+1)*100);
+	yAxLinesScaled = scale(yAxLines, 0, height);
+	yAxLinesScaled.reverse().slice(1).slice(0,-1).forEach((v,i) => {
+		c.moveTo(0, v);
+		c.lineTo(width, v);
+		c.fillText(yAxLines[i], 2,v+10);
+	});
+	c.fillText(max,2,10);
+	c.fillText(min,2,390);
+	c.stroke();
+	
+	canvas[0].addEventListener('click', function (e) {
+		// log(e.x, e.y);
+		// log( c.isPointInPath(path, e.x, e.y) );
+	});
 });
 
 function map2px(nums) {
