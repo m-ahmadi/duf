d85b08199168d19df50962d06a4da44fb52579d7
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-04-20 04:07:18 +0430

    stack4: some variables & laptop sizes in watch.cmd

--- watch.cmd
+++ watch.cmd
@@ -14,11 +14,14 @@ if %errorlevel% == 1 (echo cmdow not installed. && exit /b 1)
 setlocal EnableDelayedExpansion
 set count=0
 set top=157
+set left=1430
+set step=110
 for /f %%i in ('cmdow /t') do (
-	cmdow %%i /mov 1430 !top!
-	set /a "top=!top!+110"
+	cmdow %%i /mov !left! !top!
+	set /a "top=!top!+!step!"
 	set /a "count=!count!+1"
 	if "!count!" == "8" goto :done
 )
 :done
 setlocal DisableDelayedExpansion
+REM laptop: top=-35 left=875 step=95 (set.cmd: lines=4)
