3fb8a88f93d4a2d85d985f8ce91a817860817382
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-04-19 22:20:39 +0430

    sigdraw: added input element for y scale

--- zother/sigdraw/main.js
+++ zother/sigdraw/main.js
@@ -13,6 +13,7 @@ $(async function () {
 	graphCanvas = document.querySelector('canvas#graph');
 	graph = graphCanvas.getContext('2d');
 	yAx = document.querySelector('canvas#y-labels').getContext('2d');
+	yScaleBtn = document.querySelector('#y-scale-btn');
 	
 	graphCanvas.width = prices.length;
 	graphCanvas.height = 400;
@@ -20,10 +21,41 @@ $(async function () {
 	height = graphCanvas.height;
 	yAx.canvas.width = 50;
 	yAx.canvas.height = graphCanvas.height;
+	yScaleBtn.style.top = height;
 	
 	min = Math.min(...prices);
 	max = Math.max(...prices);
-	yScale = 80;
+	yScale = 50;
+	pricesPx = map2px(prices, height);
+	draw();
+	
+	graphCanvas.addEventListener('mousemove', function (e) {
+		// const x = e.offsetX; //e.pageX - canvas.offsetLeft;
+		// const y = e.offsetY; //e.pageY - canvas.offsetTop;
+		// log(x, y);
+		// log( graph.isPointInPath(path, x, y) );
+	});
+	
+	yScaleBtn.addEventListener('input', debounce(function () {
+		const { value, min, max } = this;
+		+value > +max && (this.value = max);
+		+value < +min && (this.value = min);
+		yScale = +this.value;
+		draw();
+	}, 100));
+	
+	yScaleBtn.addEventListener('wheel', debounce(function (e) {
+		const { deltaY } = e;
+		deltaY < 0 && this.stepUp();
+		deltaY > 0 && this.stepDown();
+		this.dispatchEvent(new Event('input'));
+	}, 20));
+});
+
+function draw() {
+	graph.clearRect(0, 0, width, height);
+	yAx.clearRect(0, 0, yAx.canvas.width, yAx.canvas.height);
+	
 	maxRem = dp(max % yScale);
 	minRem = dp(min % yScale);
 	yUpper = maxRem ? dp(max + (yScale-maxRem)) : max;
@@ -31,12 +63,11 @@ $(async function () {
 	minScale = dp(yUpper - max);
 	maxScale = dp(height - (min-yLower));
 	
-	pricesPx = map2px(prices, height);
 	pricesPxScaled = scale(pricesPx, minScale, maxScale);
 	step = 0;
 	points = pricesPxScaled.map(i => ({x: step+=1, y: i}));
 	
-	var path = new Path2D();
+	path = new Path2D();
 	// graph
 	path.moveTo(points[0].x, points[0].y);
 	points.slice(1).forEach(({x,y}) =>
@@ -54,7 +85,7 @@ $(async function () {
 	yLines = [];
 	for (let i=yLower; i<=yUpper; i+=yScale) yLines.push(i);
 	
-	log(yLines);
+	yAx.beginPath();
 	scale(map2px(yLines, height), 0, height).slice(1).slice(0,-1).forEach((v,i) => {
 		const label = yLines[i+1];
 		const idx = label.toString().length - 1;
@@ -76,14 +107,7 @@ $(async function () {
 		graph.lineTo(i, height);
 	}
 	graph.stroke();
-	
-	graphCanvas.addEventListener('mousemove', function (e) {
-		// const x = e.offsetX; //e.pageX - canvas.offsetLeft;
-		// const y = e.offsetY; //e.pageY - canvas.offsetTop;
-		// log(x, y);
-		// log( graph.isPointInPath(path, x, y) );
-	});
-});
+}
 
 function map2px(nums, height=150) {
 	const min = Math.min(...nums);
@@ -104,4 +128,13 @@ function scale(nums, newMin=0, newMax=100) {
 
 function dp(num, place=1) {
 	return +( num.toFixed(place) );
+}
+
+
+function debounce(fn, wait) {
+	let timeout
+	return function (...args) {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => fn.apply(this, args), wait);
+	};
 }
